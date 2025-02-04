bfd961e0a29d289b452a828cb3e5df326d287056
unknown <minh>
2025-02-04 20:24:24 +0700

    refactor: rebuild

--- src/main/java/com/home/spring_cpe_stats/traffic_manager/application/in/TrafficManagerUseCase.java
+++
@@ -1,9 +0,0 @@
-package com.home.spring_cpe_stats.traffic_manager.application.in;
-
-import com.home.spring_cpe_stats.traffic_manager.domain.entity.TrafficInfo;
-
-import java.util.List;
-
-public interface TrafficManagerUseCase {
-    void saveTrafficRecord(List<TrafficInfo> trafficInfoList);
-}
--- src/main/java/com/home/spring_cpe_stats/traffic_manager/domain/entity/VendorVo.java
+++
@@ -1,6 +0,0 @@
-package com.home.spring_cpe_stats.traffic_manager.domain.entity;
-
-public enum VendorVo {
-    ARUBA_IAP,
-    VNPT_IGATE
-}
